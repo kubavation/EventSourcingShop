61edf11c87340852085de9d0c0bb2fe6ac33edfb
kubavation <kubavation@wp.pl>
2019-07-27 19:46:14 +0200

    accountAggregate added

---
+++ src/main/java/io/duryskuba/EventSourcingShop/aggregate/AccountAggregate.java
@@ -0,0 +1,27 @@
+package io.duryskuba.EventSourcingShop.aggregate;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.spring.stereotype.Aggregate;
+
+import java.time.LocalDateTime;
+
+@Aggregate
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AccountAggregate {
+
+    @AggregateIdentifier
+    private String id;
+
+    private String username;
+    private String password;
+    private String email;
+
+    private LocalDateTime createdAt;
+}
+
+
