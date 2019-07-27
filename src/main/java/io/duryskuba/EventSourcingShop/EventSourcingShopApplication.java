9fc898e6a22a3868c4e9a3b844762bdae1a40966
kubavation <kubavation@wp.pl>
2019-07-27 17:58:43 +0200

    init

---
+++ src/main/java/io/duryskuba/EventSourcingShop/EventSourcingShopApplication.java
@@ -0,0 +1,13 @@
+package io.duryskuba.EventSourcingShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingShopApplication.class, args);
+	}
+
+}
