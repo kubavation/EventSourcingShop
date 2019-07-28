86c7733f4b6c292967389bfe7771dab6e6969dc7
kubavation <kubavation@wp.pl>
2019-07-28 15:46:47 +0200

    jpa repository for account - fix

--- src/main/java/io/duryskuba/EventSourcingShop/repository/AccountRepository.java
+++ src/main/java/io/duryskuba/EventSourcingShop/repository/AccountRepository.java
@@ -1,8 +1,9 @@
 package io.duryskuba.EventSourcingShop.repository;
 
+import io.duryskuba.EventSourcingShop.model.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AccountRepository extends JpaRepository<> {
+public interface AccountRepository extends JpaRepository<Account, String> {
 }
