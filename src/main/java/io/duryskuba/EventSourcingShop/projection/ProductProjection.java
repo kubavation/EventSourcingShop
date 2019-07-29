633fa7d66e7aa7a585a275c7d838c093abbdc8f9
kubavation <kubavation@wp.pl>
2019-07-29 19:25:59 +0200

    ProductProjection base class added

--- src/main/java/io/duryskuba/EventSourcingShop/projection/ProductProjection.java
+++ src/main/java/io/duryskuba/EventSourcingShop/projection/ProductProjection.java
@@ -1,7 +1,28 @@
 package io.duryskuba.EventSourcingShop.projection;
 
+import io.duryskuba.EventSourcingShop.event.product.ProductCreatedEvent;
+import io.duryskuba.EventSourcingShop.model.Product;
+import io.duryskuba.EventSourcingShop.repository.ProductRepository;
+import org.axonframework.eventhandling.EventHandler;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ProductProjection {
+
+    private final ProductRepository productRepository;
+
+    public ProductProjection(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @EventHandler
+    public void on(ProductCreatedEvent event) {
+        productRepository.save(
+                Product.builder()
+                    .id(event.getId())
+                    .name(event.getName())
+                    .code(event.getCode())
+                    .description(event.getDescription())
+                    .groupCode(event.getGroupCode()).build() );
+    }
 }
