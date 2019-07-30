2177c0f6e859478c8e9656859e55f2fd21cc42be
kubavation <kubavation@wp.pl>
2019-07-30 20:31:37 +0200

    example impl of cart

--- src/main/java/io/duryskuba/EventSourcingShop/aggregate/ShoppingCart.java
+++ src/main/java/io/duryskuba/EventSourcingShop/aggregate/ShoppingCart.java
@@ -1,11 +1,15 @@
 package io.duryskuba.EventSourcingShop.aggregate;
 
+import io.duryskuba.EventSourcingShop.enums.ShoppingCartStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.axonframework.modelling.command.AggregateIdentifier;
+import org.axonframework.modelling.command.AggregateMember;
 import org.axonframework.spring.stereotype.Aggregate;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Aggregate
@@ -17,5 +21,11 @@ public class ShoppingCart {
     @AggregateIdentifier
     private String id;
 
-    private List<ProductAggregate> products;
+    private String accountId;
+    private ShoppingCartStatus shoppingCartStatus;
+    private List<String> productIds;
+
+    private LocalDateTime createdAt;
+    
+
 }
