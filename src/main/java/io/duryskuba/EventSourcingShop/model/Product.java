de28f550ac0b1c60efb3667f022680972365e0e7
kubavation <kubavation@wp.pl>
2019-08-11 12:29:03 +0200

    some changes in product entity

--- src/main/java/io/duryskuba/EventSourcingShop/model/Product.java
+++ src/main/java/io/duryskuba/EventSourcingShop/model/Product.java
@@ -9,7 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
 
 @Entity
 @NoArgsConstructor
@@ -25,6 +28,11 @@ public class Product {
     private String description;
     private String code;
     private String groupCode;
+
+    private AtomicLong quantity;
+    private BigDecimal price;
+
+    private LocalDateTime addedAt;
 
     @ManyToMany(mappedBy = "products")
     private List<ShoppingCart> shoppingCart;
