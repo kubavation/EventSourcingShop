ceb7bf1cf27f011512dcae8265ccad223c8f68d2
kubavation <kubavation@wp.pl>
2019-08-12 18:48:02 +0200

    basic cartCommandController

---
+++ src/main/java/io/duryskuba/EventSourcingShop/controller/cart/CartCommandController.java
@@ -0,0 +1,25 @@
+package io.duryskuba.EventSourcingShop.controller.cart;
+
+import io.duryskuba.EventSourcingShop.resource.CartProductDTO;
+import io.duryskuba.EventSourcingShop.service.cart.CartCommandService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.CompletableFuture;
+
+@RestController
+public class CartCommandController {
+
+    private final CartCommandService cartCommandService;
+
+    public CartCommandController(CartCommandService cartCommandService) {
+        this.cartCommandService = cartCommandService;
+    }
+
+    @PostMapping("/carts")
+    public CompletableFuture<String> addProduct(@RequestBody CartProductDTO dto) {
+        return cartCommandService.addProduct(dto);
+    }
+}
