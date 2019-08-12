1b0b204757cc6275a0c792455464aa2e7b65f180
kubavation <kubavation@wp.pl>
2019-08-12 18:35:04 +0200

    base cartProductDTO + cartCommandService

---
+++ src/main/java/io/duryskuba/EventSourcingShop/resource/CartProductDTO.java
@@ -0,0 +1,14 @@
+package io.duryskuba.EventSourcingShop.resource;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class CartProductDTO {
+
+    private String cartId;
+    private String productId;
+}
---
+++ src/main/java/io/duryskuba/EventSourcingShop/service/cart/CartCommandService.java
@@ -0,0 +1,27 @@
+package io.duryskuba.EventSourcingShop.service.cart;
+
+import io.duryskuba.EventSourcingShop.command.account.CreateAccountCommand;
+import io.duryskuba.EventSourcingShop.command.cart.AddProductCommand;
+import io.duryskuba.EventSourcingShop.model.CartProduct;
+import io.duryskuba.EventSourcingShop.resource.AccountDTO;
+import io.duryskuba.EventSourcingShop.resource.CartProductDTO;
+import org.axonframework.commandhandling.gateway.CommandGateway;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+@Service
+public class CartCommandService {
+
+    private final CommandGateway commandGateway;
+
+    public CartCommandService(CommandGateway commandGateway) {
+        this.commandGateway = commandGateway;
+    }
+
+    public CompletableFuture<String> addProduct(CartProductDTO dto) {
+        return commandGateway.send(
+                new AddProductCommand(dto.getCartId(), dto.getProductId()));
+    }
+}
